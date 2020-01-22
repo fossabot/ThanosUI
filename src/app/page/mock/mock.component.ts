3c2412ea5700dce7ed2fb80c0720701a0d438ca1
SaraQian <abigail830@163.com>
2020-01-22 17:11:00 +0800

    adding playground

--- src/app/page/mock/mock.component.ts
+++ src/app/page/mock/mock.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MockMapping } from 'src/app/models/MockMapping';
 import { MockServerService } from 'src/app/service/mockserver.service';
 import { MockMappingDTO } from 'src/app/models/MockMappingDTO';
+import { MatPaginator } from '@angular/material/paginator';
 
-
-const ELEMENT_DATA: MockMapping[] = [
-  { provider: 'provider', consumer: 'consumer1', port: 12345 },
-  { provider: 'provider', consumer: 'consumer2', port: 12345 },
-  { provider: 'provider', consumer: 'consumer3', port: 12345 },
-  { provider: 'provider', consumer: 'consumer4', port: 12345 },
-  { provider: 'provider', consumer: 'consumer5', port: 12345 },
-  { provider: 'provider', consumer: 'consumer6', port: 12345 },
-  { provider: 'provider', consumer: 'consumer7', port: 12345 },
-  { provider: 'provider', consumer: 'consumer8', port: 12345 },
-  { provider: 'provider', consumer: 'consumer9', port: 12345 },
-  { provider: 'provider', consumer: 'consumer10', port: 12345 },
-  { provider: 'provider', consumer: 'consumer11', port: 12345 },
-  { provider: 'provider', consumer: 'consumer12', port: 12345 },
-  { provider: 'provider', consumer: 'consumer13', port: 12345 },
-];
 
 /**
  * @title Table with filtering
@@ -30,24 +15,30 @@ const ELEMENT_DATA: MockMapping[] = [
   styleUrls: ['./mock.component.scss']
 })
 export class MockComponent implements OnInit {
-  displayedColumns: string[] = ['provider', 'consumer', 'port'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  mappings: MockMapping[];
+  displayedColumns: string[] = ['provider', 'consumer', 'port'];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   constructor(mockServerService: MockServerService) {
+    this.getDataSource(mockServerService);
+  }
+
+  getDataSource(mockServerService: MockServerService) {
     mockServerService.getMapping().subscribe(response => {
-      this.mappings = response.map(res => new MockMappingDTO(res))
+      this.dataSource.data = response.map(res => new MockMappingDTO(res))
         .filter(dto => dto.isValid())
         .map(mapping => mapping.toMockMapping());
-      console.log(this.mappings);
+      console.log(this.dataSource.data);
     });
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  ngOnInit() {
   }
 }
